24b53cf8b389432784f0b82a2fe2fc62f40ba480
Arminder Singh <arminders208@outlook.com>
2024-12-19 18:40:46 -0500

    all optional parameter parsing and image processing finished, GIF support is last item on TODO list

--- src/imageprocessing.cpp
+++ src/imageprocessing.cpp
@@ -1,5 +1,8 @@
 /*++
-* 
+* Arminder Singh
+* CPE462 Image Processing Final Project
+* I pledge my Honor that I have abided by the Stevens Honor System.
+* - Arminder Singh
 --*/
 
 
@@ -19,12 +22,9 @@
    DELETEME when finished:
    Project outline:
    - Make this work with JPEGs and OpenCV first
-     - make basic image manipulation work as in HW6
-	 - do image orientation
 	 - loop code at speed (GIF max framerate is 50fps, we have 3 frames, base case is 1 frame per second)
 	 - command parsing
-	 - randomization
-   - conversion to/from formats
+	 - GIF conversion
 --*/
 
 /*++
@@ -39,19 +39,21 @@
 typedef enum {
 	MODE_COLOR_WHEEL = 0,
 	MODE_GIF_OUTPUT,
-	MODE_CONVERT_RAW,
 	MODE_MAX,
 	MODE_UNKNOWN = 0xFFFFFFFF
 } img_mode;
 
 using namespace cv; // makes it so any OpenCV methods do not need to be prefixed with "cv::"
 
+
+
+
 //
 // Color wheel mode needs these arguments (some are optional, but must be listed in the order specified):
 //   input_image - image to be used as input to the color wheel (must be JPEG). Required, must be a file path.
 //   angle - how many degrees counter-clockwise you want to rotate the image. Optional, will be 0 unless specified
-//   no_equalize_histogram - do not do histogram equalization on the image before creating the output images. 
-//   Optional, must be the fifth argument after input_image.
+//   equalize_histogram - do histogram equalization on the image before creating the output images. 
+//   Optional, ignored if incorrect.
 //   framerate - must be a number between 1 and 50 (units are frames per second). Optional, defaults to 1.
 // 
 // Assumptions:
@@ -66,8 +68,10 @@ int main_color_wheel(int argc, char* argv[]) {
 	Mat image_out_temp;
 	Mat channel_out[3]; // channels [1,2,3] image out. (TODO: could we determine color space of the image in a future iteration to make these R, G, B?)
 	Mat hsv_channel_out[3];
-	Mat mixed_image_out; // add a mixed image output
+	Mat mixed_image_out, hsv_mixed_image_out; // add a mixed image output
 	Mat rotation_matrix, affine_matrix;
+	Mat rearranged_channels[3];
+	Mat hsv_rearranged_channels[3];
 	Point2f sourceTriangle[3];
 	Point2f destTriangle[3];
 	cv::Point center_img;
@@ -80,12 +84,10 @@ int main_color_wheel(int argc, char* argv[]) {
 	const cv::String out_ch_string_3 = "out_ch_3.jpg";
 	const cv::String out_string_mixed = "out_mixed.jpg";
 	const cv::String out_gif_string = "out_gif.gif";
-	bool do_histogram_equalization = true;
-	bool do_randomize_orientation = false; //ALL randomization is disabled for now.
-	bool do_randomize_angle = false;
+	bool do_histogram_equalization = false; //do not do histogram equalization by default.
 	bool mix_colors = false; // default to not mixing colors
 	bool was_any_transform_done = false; // need to check if any transforms are done to image to choose whether to apply a transformation to original input or to the current running output.
-	unsigned int rotation_angle = 90; //default to keeping image angle as is.
+	unsigned int rotation_angle = 0; //default to keeping image angle as is.
 	unsigned int framerate = 1; //leave it as 1 frame per second.
 	char* input_file_ext;
 	//
@@ -116,19 +118,42 @@ int main_color_wheel(int argc, char* argv[]) {
 	// Optional command parsing disabled for now.
 	// 
 	
-	//if (argc >= 4) {
-	//	// parse all the "Optional" parameters here. we always enter this if we have at least one optional parameter passed in.
-	//	if (strncmp(argv[3], "no_equalize_histogram", 21) == 0) {
-	//		// we've been asked to skip histogram equalization.
-	//		do_histogram_equalization = false;
-	//	}
-	//	else if (strncmp(argv[3], "no_equalize_histogram", 21) != 0) {
-	//		//
-	//		// we have an invalid parameter where no_equalize_histogram is, exit.
-	//		//
-	//		printf("Error: ")
-	//	}
-	//}
+	//
+	// Warning: due to time constraints, I couldn't account for all of the undefined behavior here, 
+	// so please be careful when specifying parameters.
+	//
+	if (argc >= 4) {
+		// parse all the "Optional" parameters here. we always enter this if we have at least one optional parameter passed in.
+		if (strncmp(argv[3], "0", 1) != 0) {
+			try {
+				// custom angle is specified, set it.
+				printf("Info: setting rotation angle\n");
+				rotation_angle = stoi(argv[3]);	
+			}
+			catch (std::invalid_argument const& ex) {
+				printf("Error: Third argument is not an integer\n");
+				//print_help();
+				return -1;
+			}
+		}
+		if (strncmp(argv[4], "equalize_histogram", 21) == 0) {
+			printf("Info: doing histogram equalization\n");
+			do_histogram_equalization = true;
+		}
+		if (strncmp(argv[5], "0", 1) != 0) {
+			try {
+				// custom framerate is specified, set it.
+				printf("Info: setting rotation angle\n");
+				framerate = stoi(argv[5]);	
+			}
+			catch (std::invalid_argument const& ex) {
+				printf("Error: Fifth argument is not an integer\n");
+				//print_help();
+				return -1;
+			}
+		}
+		
+	}
 
 	image_in = imread(in_string, cv::IMREAD_COLOR);
 	if (image_in.empty()) {
@@ -165,17 +190,6 @@ int main_color_wheel(int argc, char* argv[]) {
 		}
 	}
 
-
-	//
-	// Do histogram equalization, if the user requested it.
-	//
-	if (do_histogram_equalization == true) {
-		//cv::equalizeHist(((was_any_transform_done == true) ? image_out_temp : image_in), image_out_temp);
-		if (was_any_transform_done == false) {
-			was_any_transform_done = true;
-		}
-	}
-
 	//
 	// extract the image channels, and dump them out to the disk as JPG.
 	//
@@ -184,6 +198,15 @@ int main_color_wheel(int argc, char* argv[]) {
 	cv::imwrite(out_ch_string_1, channel_out[0]);
 	cv::imwrite(out_ch_string_2, channel_out[1]);
 	cv::imwrite(out_ch_string_3, channel_out[2]);
+
+	//
+	// Do histogram equalization on the channels, if the user requested it.
+	//
+	if (do_histogram_equalization == true) {
+		cv::equalizeHist(channel_out[0], channel_out[0]);
+		cv::equalizeHist(channel_out[1], channel_out[1]);
+		cv::equalizeHist(channel_out[2], channel_out[2]);
+	}
 
 
 	//
@@ -207,8 +230,35 @@ int main_color_wheel(int argc, char* argv[]) {
 	// Mix the channels from earlier into a new BGR8 image.
 	//
 
+	unsigned int first_channel = (rand() % 3);
+	switch(first_channel) {
+		case 0:
+			rearranged_channels[0] = channel_out[0];
+			rearranged_channels[1] = channel_out[2];
+			rearranged_channels[2] = channel_out[1];
+			hsv_rearranged_channels[0] = hsv_channel_out[0];
+			hsv_rearranged_channels[1] = hsv_channel_out[2];
+			hsv_rearranged_channels[2] = hsv_channel_out[1];
+			break;
+		case 1:
+			rearranged_channels[0] = channel_out[1];
+			rearranged_channels[1] = channel_out[2];
+			rearranged_channels[2] = channel_out[0];
+			hsv_rearranged_channels[0] = hsv_channel_out[1];
+			hsv_rearranged_channels[1] = hsv_channel_out[2];
+			hsv_rearranged_channels[2] = hsv_channel_out[0];
+			break;
+		case 2:
+			rearranged_channels[0] = channel_out[2];
+			rearranged_channels[1] = channel_out[0];
+			rearranged_channels[2] = channel_out[1];
+			hsv_rearranged_channels[0] = hsv_channel_out[2];
+			hsv_rearranged_channels[1] = hsv_channel_out[0];
+			hsv_rearranged_channels[2] = hsv_channel_out[1];
+	}
 
-
+	cv::merge(rearranged_channels, mixed_image_out);
+	cv::merge(hsv_rearranged_channels, hsv_mixed_image_out);
 	//
 	// Combine the frames into a GIF.
 	//
@@ -229,7 +279,7 @@ int main(int argc, char *argv[])
 
 	//
 	// Mode selector:
-	// Valid modes are 'color_wheel', 'output_as_gif', 'convert_raw' for now.
+	// Valid modes are 'color_wheel' and 'output_as_gif for now.
 	//
 	// If we don't have a valid mode selected, since mode is default set to UNKNOWN, exit once we check for modes.
 	//
@@ -248,9 +298,6 @@ int main(int argc, char *argv[])
 	else if (strncmp(argv[1], "output_as_gif", 13) == 0) {
 		mode = MODE_GIF_OUTPUT;
 	}
-	else if (strncmp(argv[1], "convert_raw", 11) == 0) {
-		mode = MODE_CONVERT_RAW;
-	}
 
 	//
 	// Check our running mode at this point. If unknown, exit.
@@ -265,137 +312,14 @@ int main(int argc, char *argv[])
 			printf("Running in color wheel mode\n");
 			ret = main_color_wheel(argc, argv);
 			break;
+		case MODE_GIF_OUTPUT:
+			printf("Converting images to GIF\n");
+			//ret = main_convert_to_gif();
 
 	}
 
 	return ret;
 
-	//
-	// Temporarily disabling the RAW image code
-	//
-#if 0
-	FILE  *in, *out;
-	int   j, k, width, height;
-	int ** image_in, ** image_out;
-	float sum1, sum2;
-	float new_T, old_T, delta_T;
-	long count1, count2;
-	errno_t err;
-
-	if(argc<5) { printf("ERROR: Insufficient parameters!\n"); return(1);}
-
-	width = atoi(argv[3]);
-	height = atoi(argv[4]);
-
-	image_in = (int**) calloc(height, sizeof(int*));
-	if(!image_in)
-	{
-		printf("Error: Can't allocate memmory!\n");
-		return(1);
-	}
-
-	image_out = (int**) calloc(height, sizeof(int*));
-	if(!image_out)
-	{
-		printf("Error: Can't allocate memmory!\n");
-		return(1);
-	}
-
-	for (j=0; j<height; j++)
-	{
-		image_in[j] = (int *) calloc(width, sizeof(int));
-		if(!image_in[j])
-		{
-			printf("Error: Can't allocate memmory!\n");
-			return(1);
-		}
-
-		image_out[j] = (int *) calloc(width, sizeof(int));
-		if(!image_out[j])
-		{
-			printf("Error: Can't allocate memmory!\n");
-			return(1);
-		}
-
-	}
-
-	if((err=fopen_s(&in, argv[1],"rb"))!=0)
-	{
-		printf("ERROR: Can't open in_file!\n");
-		return(1);
-	}
-
-	if((err=fopen_s(&out, argv[2],"wb"))!=0)
-	{
-		printf("ERROR: Can't open out_file!\n");
-		return(1);
-	}
-
-	for (j=0; j<height; j++)
-		for (k=0; k<width; k++)
-	    	{
-			if((image_in[j][k]=getc(in))==EOF)
-			{
-				printf("ERROR: Can't read from in_file!\n");
-				return(1);
-		      }
-	    	}
-      if(fclose(in)==EOF)
-	{
-		printf("ERROR: Can't close in_file!\n");
-		return(1);
-	}
-
-#endif
-
-/********************************************************************/
-/* Image Processing begins                                          */
-/********************************************************************/
-
-#if 0
-	for (j=0; j<height; j++)
-		for (k=0; k<width; k++)
-	    	{
-			image_out[j][k]=255-image_in[j][k];
-		}
-#endif
-
-
-
-/********************************************************************/
-/* Image Processing ends                                          */
-/********************************************************************/
-
-#if 0
-
-	/* save image_out into out_file in RAW format */
-	for (j=0; j<height; j++)
-		for (k=0; k<width; k++)
-	    {
-	            if((putc(image_out[j][k],out))==EOF)
-	            {
-		        	printf("ERROR: Can't write to out_file!\n");
-				    return(1);
-	            }
-		}
-
-    if(fclose(out)==EOF)
-	{
-		printf("ERROR: Can't close out_file!\n");
-		return(1);
-	}
-
-
-	for (j=0; j<height; j++)
-	{
-		free(image_in[j]);
-		free(image_out[j]);
-	}
-	free(image_in);
-	free(image_out);
-#endif
-
-    return 0;
 }
 
 
